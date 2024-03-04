beb119fd1e21a15fd4023a517e7c4b89bca18457
filipecosta90 <filipecosta.90@gmail.com>
2024-03-04 12:32:57 +0000

    Extra logging on which auth method we use on elastic

--- engine/clients/elasticsearch/config.py
+++ engine/clients/elasticsearch/config.py
@@ -25,12 +25,14 @@ def get_es_client(host, connection_params):
         url = "http://"
     url += f"{host}:{ELASTIC_PORT}"
     if ELASTIC_API_KEY is None:
+        print("Connecting using basic auth")
         client = Elasticsearch(
             url,
             basic_auth=(ELASTIC_USER, ELASTIC_PASSWORD),
             **init_params,
         )
     else:
+        print("Connecting using api key")
         client = Elasticsearch(
             url,
             api_key=ELASTIC_API_KEY,
