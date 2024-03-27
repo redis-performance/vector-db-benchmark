eb8e6d471c8ea171f492c55695c00b29854a0928
filipecosta90 <filipecosta.90@gmail.com>
2024-03-27 14:06:05 +0000

    Changes towards memorydb compatibility

--- engine/clients/redis/config.py
+++ engine/clients/redis/config.py
@@ -6,5 +6,5 @@ REDIS_USER = os.getenv("REDIS_USER", None)
 REDIS_CLUSTER = bool(int(os.getenv("REDIS_CLUSTER", 0)))
 REDIS_HYBRID_POLICY = os.getenv("REDIS_HYBRID_POLICY", None)
 
-# 90 seconds timeout
+# 60 seconds timeout
 REDIS_QUERY_TIMEOUT = int(os.getenv("REDIS_QUERY_TIMEOUT", 60 * 1000))
