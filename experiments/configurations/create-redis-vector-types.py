a3284f82910e229262196923eb0ee18499193194
filipecosta90 <filipecosta.90@gmail.com>
2024-07-12 06:53:59 +0100

    Added more lower M configs for redis variations

--- experiments/configurations/create-redis-vector-types.py
+++ experiments/configurations/create-redis-vector-types.py
@@ -3,9 +3,9 @@ import json
 experiments = []
 
 for data_type in ["FLOAT16", "BFLOAT16", "FLOAT32", "FLOAT64"]:
-    for m in [16, 32, 64]:
-        for efConstruction in [32, 64]:
-        #for efConstruction in [32, 64, 128, 256, 512]:
+    for m in [8, 16, 32, 64]:
+        # for efConstruction in [32, 64]:
+        for efConstruction in [16, 32, 64, 128, 256, 512]:
             search_params = []
             config = {
                 "name": f"redis-{data_type.lower()}-m-{m}-ef-{efConstruction}",
@@ -18,8 +18,8 @@ for data_type in ["FLOAT16", "BFLOAT16", "FLOAT32", "FLOAT64"]:
                 "search_params": [],
                 "upload_params": {"parallel": 16, "data_type": data_type},
             }
-#            for efSearch in [16, 32, 1024]:
-            for efSearch in [16, 32, 64, 128, 256, 512, 1024]:
+            #            for efSearch in [16, 32, 1024]:
+            for efSearch in [8, 16, 32, 64, 128, 256, 512, 1024]:
                 single_client_config = {
                     "parallel": 1,
                     "search_params": {"ef": efSearch, "data_type": data_type},
