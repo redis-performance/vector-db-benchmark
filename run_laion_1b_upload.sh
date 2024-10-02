2d8e250746a831de8ebdd9625cc9a3278b1f3e8c
filipecosta90 <filipecosta.90@gmail.com>
2024-10-02 13:28:26 +0100

    Fixed some typos on loader script

--- run_laion_1b_upload.sh
+++ run_laion_1b_upload.sh
@@ -12,5 +12,5 @@ for i in {0..99}; do
   end_idx=$(((i + 1) * part_size))
 
   # Launch each process in a new screen session
-  screen -dmS loader_$i bash -c "REDIS_PORT=30001 REDIS_CLUSTER=1  python3 run.py --host 192.168.2.6  --engines redis-intel-float16-hnsw-m-16-ef-32 --dataset laion-img-emb-768d-1Billion-cosine  --skip-search --upload_start_idx $start_idx --upload_end_idx $end_idx --part $((i + 1))"
+  screen -dmS loader_$i bash -c "REDIS_PORT=30001 REDIS_CLUSTER=1  python3 run.py --host 192.168.2.6  --engines redis-intel-float16-hnsw-m-16-ef-32 --datasets laion-img-emb-768d-1Billion-cosine  --skip-search --upload-start-idx $start_idx --upload-end-idx $end_idx "
 done
