85c8ed14460b0f88e38607014276030c0945b256
filipecosta90 <filipecosta.90@gmail.com>
2025-03-26 17:48:05 +0000

    Updated weaviate client to use grpc

--- engine/clients/weaviate/config.py
+++ engine/clients/weaviate/config.py
@@ -1,24 +1,24 @@
 import os
-from weaviate import Client
-from weaviate.auth import AuthApiKey
+from weaviate import WeaviateClient, ConnectionParams
 
 WEAVIATE_CLASS_NAME = "Benchmark"
-WEAVIATE_DEFAULT_PORT = 8090
+WEAVIATE_DEFAULT_HTTP_PORT = 8080
+WEAVIATE_DEFAULT_GRPC_PORT = 50051
 WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY", None)
-WEAVIATE_PORT = os.getenv("WEAVIATE_PORT", WEAVIATE_DEFAULT_PORT)
+WEAVIATE_HTTP_PORT = os.getenv("WEAVIATE_HTTP_PORT", WEAVIATE_DEFAULT_HTTP_PORT)
+WEAVIATE_GRPC_PORT = os.getenv("WEAVIATE_GRPC_PORT", WEAVIATE_DEFAULT_GRPC_PORT)
 
 
 def setup_client(connection_params, host):
-    port = connection_params.get("port", WEAVIATE_PORT)
+    port = connection_params.get("port", WEAVIATE_HTTP_PORT)
     if host.startswith("http"):
         url = ""
     else:
         url = "http://"
     url += f"{host}:{port}"
-    auth_client_secret = None
-    if WEAVIATE_API_KEY is not None:
-        auth_client_secret = AuthApiKey(WEAVIATE_API_KEY)
-    c = Client(url, auth_client_secret, **connection_params)
+    c = WeaviateClient(
+        ConnectionParams.from_url(url, WEAVIATE_GRPC_PORT), skip_init_checks=True
+    )
     # Ping Weaviate's live state.
     assert c.is_live() is True
     return c
