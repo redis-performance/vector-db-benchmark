e4d93905098c2e1c4dbca8af1ce9b2adf21b494f
filipecosta90 <filipecosta.90@gmail.com>
2025-03-31 13:09:57 +0100

    catching 2.10 error message. setting default to flushall to mimic competiton

--- engine/clients/redis/config.py
+++ engine/clients/redis/config.py
@@ -7,11 +7,11 @@ REDIS_CLUSTER = bool(int(os.getenv("REDIS_CLUSTER", 0)))
 # One of BATCHES and ADHOC_BF
 # check https://redis.io/docs/latest/develop/interact/search-and-query/advanced-concepts/vectors/#pre-filter-query-attributes-hybrid-approach
 REDIS_HYBRID_POLICY = os.getenv("REDIS_HYBRID_POLICY", "")
-REDIS_KEEP_DOCUMENTS = bool(os.getenv("REDIS_KEEP_DOCUMENTS", 1))
+REDIS_KEEP_DOCUMENTS = bool(os.getenv("REDIS_KEEP_DOCUMENTS", 0))
 REDIS_JUST_INDEX = bool(os.getenv("REDIS_JUST_INDEX", 0))
 GPU_STATS = bool(int(os.getenv("GPU_STATS", 0)))
 GPU_STATS_ENDPOINT = os.getenv("GPU_STATS_ENDPOINT", None)
 
 
 # 60 seconds timeout
-REDIS_QUERY_TIMEOUT = int(os.getenv("REDIS_QUERY_TIMEOUT", 60 * 1000))
+REDIS_QUERY_TIMEOUT = int(os.getenv("REDIS_QUERY_TIMEOUT", 90 * 1000))
