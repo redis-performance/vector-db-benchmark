0590e976d146f56a6af508f02e3a69e45d185eeb
Marcin Poźniak <marcin.pozniak@intel.com>
2025-06-26 20:07:09 +0200

    Dbpedia configuration (#28)
    * Add dbpedia calibration json
    
    * Add LVQ's to dbpedia-calibration configuration
    
    * Fix create-svs
    
    * Add LeanVec4X8

--- experiments/configurations/create-svs.py
+++ experiments/configurations/create-svs.py
@@ -1,14 +1,14 @@
 import json
 
 threads = [16]
-ws_constructs = [100]
-ws_search = [32, 40, 48, 64]
+ws_constructs = [200]
+ws_search = [177]
 #ws_search = [48]
 graph_degree = [32]
-#quantization = ["NO_COMPRESSION", "LVQ4x4", "LVQ4x8", "LVQ4", "LVQ8", "LeanVec4x8", "LeanVec8x8"]
-quantization = ["LVQ4x8"]
-topKs = [10]
-data_types = ["FLOAT32"]
+#quantization = ["LVQ4X4", "LVQ4x8", "LVQ8", "LVQ4"]
+quantization = ["LVQ4X4"]
+topKs = [100]
+data_types = ["FLOAT16", "FLOAT32"]
 
 for algo in ["svs-vamana"]:
     for data_type in data_types:
@@ -18,34 +18,34 @@ for algo in ["svs-vamana"]:
                     configs = []
                     for thread in threads:
                         config = {
-                            "name": f"svs-test-algo-{algo}-graph-{graph_d}-ws-con-{ws_construct}-quant-{quant}-threads-{thread}-dt-{data_type}",
+                            "name": f"svs-{algo}-quant-{quant}-dt-{data_type}",
                             "engine": "redis",
                             "connection_params": {},
                             "collection_params": {
                                 "algorithm": algo,
                                 "data_type": data_type,
-                                f"{algo}_config": {"NUM_THREADS": thread, "GRAPH_MAX_DEGREE": graph_d, "CONSTRUCTION_WINDOW_SIZE": ws_construct, "COMPRESSION": quant},
+                                f"{algo}_config": {"NUM_THREADS": thread, "GRAPH_MAX_DEGREE": graph_d, "CONSTRUCTION_WINDOW_SIZE": ws_construct, "compression": quant},
                             },
                             "search_params": [],
                             "upload_params": {
-                                "parallel": 128,
+                                "parallel": 100,
                                 "data_type": data_type,
                                 "algorithm": algo,
                             },
                         }
-                        for client in [1, 8, 16, 32, 64, 128]:
+                        for client in [100]:
                             for ws_s in ws_search:
                                 for top in topKs:
                                     test_config = {
                                         "algorithm": algo,
                                         "parallel": client,
                                         "top": top,
-                                        "search_params": {"SEARCH_WINDOW_SIZE": ws_s, "data_type": data_type},
+                                        "search_params": {"WS_SEARCH": ws_s, "data_type": data_type},
                                     }
                                     config["search_params"].append(test_config)
                         configs.append(config)
 
-                    fname = f"svs-test-algo-{algo}-graph-{graph_d}-ws-con-{ws_construct}-quant-{quant}-threads-{thread}-dt-{data_type}.json"
+                    fname = f"svs-{algo}-quant-{quant}-dt-{data_type}.json"
                     with open(fname, "w") as json_fd:
                         json.dump(configs, json_fd, indent=2)
                         print(f"Created {len(configs)} configs for {fname}.")
